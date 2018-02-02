0dc9cca447a590048dd33595a26fd68404da7d50
prakashutoledo <coolboyz33prakash@gmail.com>
2018-02-02 15:22:12 -0500

    Minor changes
    Just a simple hash table.

--- src/spellcheckergui/HashTable.java
+++ src/spellcheckergui/HashTable.java
@@ -205,14 +205,4 @@ public class HashTable<K, V> implements HashTableInterface<K,V> {
         }
         return numDupes;
     }
-    public boolean isPrime(int n) {
-        if (n%2==0) 		
-           return false;
-        else
-           for(int i=3; i*i<=n; i+=2) {
-              if(n%i==0)
-                 return false;
-           }
-        return true;
-    }
 }
